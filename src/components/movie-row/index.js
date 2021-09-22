be620e2207f5deee422ab727c5016f2ef61d1d03
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2021-09-22 18:32:19 -0300

    Alterando o carrosel de videos

--- src/components/movie-row/index.js
+++ src/components/movie-row/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Movie from '../movie';
 import Video from '../../assets/unlock-background-video.mp4';
-import Carousel from 'react-elastic-carousel';
+import { Carousel } from '3d-react-carousal';
 import './movie-row.css'
 
 
@@ -19,30 +19,30 @@ export default () => {
                 { width: 1, itemsToShow: 1 },
                 { width: 550, itemsToShow: 1, itemsToScroll: 1 },
                 { width: 768, itemsToShow: 1 },
-                { width: 1200, itemsToShow: 2 }
+                { width: 1200, itemsToShow: 1 }
         ];
+
+        let slides = [
+
+                <Movie url={Video}></Movie>,
+                <Movie url={Video}></Movie>,
+                <Movie url={Video}></Movie>,
+
+        ];
+
 
         return (
                 <div className={classes.carouselWrapper}>
-                        <Carousel className={classes.carousel} breakPoints={breakPoints}>
-                                {filmArr.map((url, key) => (
-                                        <div key={key}>
-                                                <Movie url={Video}></Movie>
-                                        </div>
-                                ))}
+                        <Carousel className={classes.carousel} slides={slides}>
                         </Carousel>
                 </div>
         );
 }
 
-/* <div className={classes.moveLeft} onClick={handleLeftArrow}>
-                                <NavigateBeforeIcon style={{ fontSize: 50 }} />
-                        </div>
-                        <div className={classes.moveRight} onClick={handleRightArrow}>
-                                <NavigateNextIcon style={{ fontSize: 50 }} />
-                        </div>
-                        {filmArr.map((url, key) => (
-                                <div key={key}>
-                                        <Movie url={Video}></Movie>
-                                </div>
-                        ))} */
+/* <Carousel className={classes.carousel} breakPoints={breakPoints}>
+                                {filmArr.map((url, key) => (
+                                        <div key={key}>
+                                                <Movie url={Video}></Movie>
+                                        </div>
+                                ))}
+                        </Carousel> */
