345adddf82a8076f59a32b274fbc46edf3ba7db1
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2021-09-23 17:20:44 -0300

    Adicionando botões com o instagram

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,11 +4,13 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Banner from '../../components/banner';
 import InfoIcon from '@material-ui/icons/Info';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import bgImage from '../../assets/home-banner.jpg';
 import brothersBanner from '../../assets/brothers-banner.jpg';
 import Lucas from '../../assets/lucas.jpg'
 import Matheus from '../../assets/matheus.jpg'
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import { Link } from "react-router-dom";
 const axios = require('axios');
 
 const useStyles = makeStyles((theme) => ({
@@ -62,7 +64,11 @@ const useStyles = makeStyles((theme) => ({
                 paddingInline: theme.spacing(3),
                 [theme.breakpoints.down('sm')]: {
                         paddingBlock: theme.spacing(3),
-                }
+                },
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
         },
         picture: {
                 width: '50%',
@@ -74,6 +80,22 @@ const useStyles = makeStyles((theme) => ({
         textBlockTypography: {
                 textAlign: 'center',
                 paddingBottom: theme.spacing(5)
+        },
+        instagramButton: {
+                fontSize: '1.5rem',
+                textTransform: 'none',
+                color: 'white!important',
+                backgroundColor: 'black',
+                '&:hover': {
+                        backgroundColor: '#444'
+                }
+        },
+        instagramIconFont: {
+                fontSize: '2rem!important'
+        },
+        linkItem: {
+                textDecoration: 'none',
+                color: 'white!important',
         }
 }))
 
@@ -139,6 +161,7 @@ export default () => {
                                                 <Typography className={classes.textBlockTypography} variant='h3'>Lucas Yuji</Typography>
                                                 <Typography className={classes.textBlockTypography} variant="h5">Shabby chic etsy cray listicle flexitarian taiyaki. Pop-up flannel occupy, tumeric slow-carb meh kombucha. Edison bulb meditation sustainable, meggings hexagon godard forage farm-to-table activated charcoal vape polaroid. Whatever ennui live-edge, hella franzen small batch wayfarers narwhal kombucha man bun 90's lo-fi. Hashtag raclette pickled knausgaard shaman bushwick lo-fi vaporware offal intelligentsia cardigan.</Typography>
                                                 <Typography className={classes.textBlockTypography} variant="h5">Portland bitters gluten-free vinyl. Celiac succulents shoreditch prism. Neutra readymade hoodie raw denim echo park post-ironic air plant chillwave cray typewriter fashion axe adaptogen. Disrupt microdosing try-hard thundercats church-key subway tile narwhal chicharrones. Tattooed knausgaard before they sold out, skateboard selfies ennui beard la croix truffaut live-edge banjo squid tumeric yuccie. Deep v post-ironic brooklyn flexitarian freegan, gochujang health goth tacos la croix keytar air plant echo park kinfolk hexagon wayfarers.</Typography>
+                                                <Button className={classes.instagramButton}  variant="contained" startIcon={<InstagramIcon className={classes.instagramIconFont} fontSize='inherit' />}><Link className={classes.linkItem} to={{ pathname: "https://www.instagram.com/_lucasyuji/" }}  target="_blank">@_lucasyuji</Link></Button>
                                         </div>
                                         <img className={classes.picture} src={Lucas}></img>
                                 </div>
@@ -148,6 +171,7 @@ export default () => {
                                                 <Typography className={classes.textBlockTypography} variant='h3'>Matheus Kenji</Typography>
                                                 <Typography className={classes.textBlockTypography} variant="h5">Shabby chic etsy cray listicle flexitarian taiyaki. Pop-up flannel occupy, tumeric slow-carb meh kombucha. Edison bulb meditation sustainable, meggings hexagon godard forage farm-to-table activated charcoal vape polaroid. Whatever ennui live-edge, hella franzen small batch wayfarers narwhal kombucha man bun 90's lo-fi. Hashtag raclette pickled knausgaard shaman bushwick lo-fi vaporware offal intelligentsia cardigan.</Typography>
                                                 <Typography className={classes.textBlockTypography} variant="h5">Portland bitters gluten-free vinyl. Celiac succulents shoreditch prism. Neutra readymade hoodie raw denim echo park post-ironic air plant chillwave cray typewriter fashion axe adaptogen. Disrupt microdosing try-hard thundercats church-key subway tile narwhal chicharrones. Tattooed knausgaard before they sold out, skateboard selfies ennui beard la croix truffaut live-edge banjo squid tumeric yuccie. Deep v post-ironic brooklyn flexitarian freegan, gochujang health goth tacos la croix keytar air plant echo park kinfolk hexagon wayfarers.</Typography>
+                                                <Button className={classes.instagramButton}  variant="contained" startIcon={<InstagramIcon className={classes.instagramIconFont} fontSize='inherit' />}><Link className={classes.linkItem} to={{ pathname: "https://www.instagram.com/kenji.films/" }}  target="_blank">@kenji.films</Link></Button>
                                         </div>
                                 </div>
                         </section>
